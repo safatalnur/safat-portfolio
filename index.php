ca104254379973d54afc11ea0ba4556ca24b7f6e
Safat Alnur <safat@macbookpro.home>
2026-02-09 00:40:45 -0500

    FEATURE: add main body parts and initial sections separation

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
-    <h1>Hello World</h1>
+<div id="main-body" class="desc-container">
+    <!-- About section -->
+    <section id="about">
+        <h2>About Me</h2>
+        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
+        <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. </p>
+        <p>The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+    </section>
+    <!-- Work experience section -->
+    <section id="experience">
+        <h2>Experience</h2>
+    </section>
+    <!-- My Work presentation section -->
+    <section id="myWork">
+        <h2>My Works</h2>
+    </section>
+</div>
 <?php get_footer(); ?>
