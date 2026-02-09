f778680a3cb7bb852bfda17f357db5f94ba8de51
Safat Alnur <safat@macbookpro.home>
2026-02-09 00:07:45 -0500

    STYLE: add google font intel and updatevariables styling

--- functions.php
+++ functions.php
@@ -12,4 +12,16 @@ function alnur_scripts() {
 
 add_action('wp_enqueue_scripts', 'alnur_scripts');
 
+function alnur_enqueue_google_font_preconnect() {
+    wp_enqueue_style('preconnect', 'https://fonts.googleapis.com', array(), null);
+    wp_enqueue_style('preconnect-gstatic', 'https://fonts.gstatic.com', array(), null);
+}
+
+add_action('wp_enqueue_scripts', 'alnur_enqueue_google_font_preconnect');
+
+function alnur_enqueue_google_inter_font() {
+    wp_enqueue_style('inter-font', 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap', array(), null);
+}
+
+add_action('wp_enqueue_scripts', 'alnur_enqueue_google_inter_font');
 ?>
